9ac8dab24576878e354d0a2db6420787e5d13a80
Olly Rybak <ollyrybak@gmail.com>
2021-07-14 18:37:25 +1000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeagueLoadout
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<RiotConnectionService>();
+
+                client.Riot = RiotSharp.RiotApi.GetDevelopmentInstance("***REMOVED***");
+            }
+
+            MainWindow window = new MainWindow();
+            window.Title = "League Loadout";
+            window.Show();
+            Console.WriteLine("Testing");
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<RiotConnectionService>()
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ Services/RiotConnectionService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RiotSharp;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace LeagueLoadout
+{
+    class RiotConnectionService
+    {
+        public RiotApi Riot;
+
+        public RiotConnectionService()
+        {
+
+        }
+
+        public async void RequestAuth()
+        {
+            var client = new HttpClient();
+
+            HttpResponseMessage response = await client.PostAsync("https://127.0.0.1:2999/swagger/v3/openapi.json");
+        }
+    }
+}
